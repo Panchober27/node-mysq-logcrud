6568d30fb8b9af9ab1878d526ec92d02391d2042
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-24 18:47:26 -0300

    Crenado Vistas y añadiendo CDN'S

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // pool es la conexion a la bd
-const pool = require('../database');
+const pool = require("../database");
 
-
-
-router.get('/add', (req,res) => {
-    res.render('links/add');
-})
+router.get("/add", (req, res) => {
+  res.render("links/add");
+});
 
 module.exports = router;

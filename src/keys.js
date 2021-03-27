1981477441777c11cbc9f319631b62b50754bee2
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-26 22:21:54 -0300

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,19 @@
+// Local DB 😁
+// module.exports = {
+//   database: {
+//     host: "localhost",
+//     user: "root",
+//     password: "admin",
+//     database: "database_links",
+//   },
+// };
+
+// Remote DB ❤
 module.exports = {
   database: {
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "database_links",
+    host: "bwr2s3zznaqwla2haa4p-mysql.services.clever-cloud.com",
+    user: "uimahelowjezntse",
+    password: "OLyZg92y0g6OAVxNUJB2",
+    database: "bwr2s3zznaqwla2haa4p",
   },
 };

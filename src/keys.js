629fa446383b3b1d8d4aa476371ae5f7c0f1576a
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-04-08 20:02:22 -0400

    Cambio las keys a unas localKeys

--- src/keys.js
+++ src/keys.js
@@ -1,19 +1,9 @@
 // Local DB 😁
-// module.exports = {
-//   database: {
-//     host: "localhost",
-//     user: "root",
-//     password: "admin",
-//     database: "database_links",
-//   },
-// };
-
-// Remote DB ❤
 module.exports = {
   database: {
-    host: "bwr2s3zznaqwla2haa4p-mysql.services.clever-cloud.com",
-    user: "uimahelowjezntse",
-    password: "OLyZg92y0g6OAVxNUJB2",
-    database: "bwr2s3zznaqwla2haa4p",
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "database_links",
   },
 };
